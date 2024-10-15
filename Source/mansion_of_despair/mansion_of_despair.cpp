a111aa15a0628e36b936168380a6e7a6b3280d9f
kurous <gugles.radu@gmail.com>
2024-10-15 23:24:22 +0200

    ue project created

---
+++ Source/mansion_of_despair/mansion_of_despair.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "mansion_of_despair.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mansion_of_despair, "mansion_of_despair" );
